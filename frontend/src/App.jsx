d0efefed5e8787edf509c7f352be0319d9a3fe37
Harshvardhan <shreeharshpb11@gmail.com>
2023-12-31 00:51:55 +0530

    create pages and added routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,24 @@
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Work from "./pages/Work";
+import About from "./pages/About";
+import Blog from "./pages/Blog";
+import SignUp from "./pages/SignUp";
+import SingIn from "./pages/SingIn";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Work />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/sign-in" element={<SingIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
